b2714be9d4908f00a3dfc7307e4ce8c5ae9ffc17
Rccyl <Raashid224@aol.com>
2015-01-08 09:11:35 -0500

    Update Tiles.java

--- Game_Files/Tiles.java
+++ Game_Files/Tiles.java
@@ -2,21 +2,21 @@ import java.until.*;
 
 public class Tiles{
       
-  /// Lumber(0),Grain(1),Ore(2),Wool(3),Brick(4)
+  /// Lumber(0),Grain(1),Ore(2),Wool(3),Brick(4),Barren(5)
   
-  protected int[]production = new int[5];
+  protected int biome;
     
-  public void Tiles(int resource){
-    production[resource]=1;
+  public void Tiles(int biome){
+    this.biome=biome;
     }
     
   public void Tiles(){
     Random rand=new Random
-    prodiction[rand.nextInt(4)]=1;
+    biome = rand.nextInt(6);
     }
    
-  public int[] getProduction(){
-    return (index of 1)
+  public int getBiome(){
+    return biome;
     }
   
 }
